6bfeacd74d6b51f971ac188222b779c6ac9d54f2
Daniel Ayala <dayala+github@protonmail.com>
2019-08-30 21:28:38 +0000

    Added edit/update functionality for grams. Added corresponsing RSpec testing.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,24 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      return render_not_found if @gram.blank?
+    end
+  end
 
   def index
   end
@@ -17,8 +36,20 @@ class GramsController < ApplicationController
     end
   end
 
+  def show
+    #@gram = Gram.find(params[:id])
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      return render_not_found if @gram.blank?
+    end
+  end
+
   private 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
 end
